4528923161231b3fc09f0eadcf532dbdf216c6a1
Roberto Franco <roberto.zetinca@outlook.com>
2020-07-12 18:48:54 -0500

    Center services section

--- pages/home/components/Services.tsx
+++ pages/home/components/Services.tsx
@@ -21,27 +21,29 @@ const Services = ({
 }) => {
   if (!services) return null;
   return (<div className='services'>
-    <h2 className='services__title'>Nuestros servicios</h2>
-    <Container>
-      <Row gutter={16} justify='center'>
-        {
-          services.map((service: HomeService) => <Col xs={24} md={12} lg={6}>
-            <div className='services__service'>
-              <div className='services__service__link'>
-                <div className='services__service__link-href'>
-                  <Link href='/'>
-                    <FontAwesomeIcon icon={service.icon} color='#37abbd' />
-                  </Link>
+    <div>
+      <h2 className='services__title'>Nuestros servicios</h2>
+      <Container>
+        <Row gutter={16} justify='center'>
+          {
+            services.map((service: HomeService) => <Col key={service.title} xs={24} md={12} lg={6}>
+              <div className='services__service'>
+                <div className='services__service__link'>
+                  <div className='services__service__link-href'>
+                    <Link href='/'>
+                      <FontAwesomeIcon icon={service.icon} color='#37abbd' />
+                    </Link>
+                  </div>
                 </div>
+                <h3 className='services__service__title'>{service.title}</h3>
+                <p className='services__service__description'>{service.description}</p>
+                <AppButton className='services__link' type='link'>{service.actionText}</AppButton>
               </div>
-              <h3 className='services__service__title'>{service.title}</h3>
-              <p className='services__service__description'>{service.description}</p>
-              <AppButton className='services__link' type='link'>{service.actionText}</AppButton>
-            </div>
-          </Col>)
-        }
-      </Row>
-    </Container>
+            </Col>)
+          }
+        </Row>
+      </Container>
+    </div>
   </div>)
 }
 
